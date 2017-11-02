fd4a556c603567d6a8ae2de96e3b191a37dbb9dc
Daniel Haneul Lim <Daniel Haneul Lim@DESKTOP-VRG1CC1>
2017-11-01 19:54:17 -0700

    Adding test class - GitHubTest.

---
+++ Program/src/GitHubTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Daniel Haneul Lim
+ *
+ */
+public class GitHubTest {
+
+	/**
+	 * 
+	 */
+	public GitHubTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
