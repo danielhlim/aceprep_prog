c3c2ee91966dda25b6ac44eaa9301ae08bd7304b
Daniel Haneul Lim <Daniel Haneul Lim@DESKTOP-VRG1CC1>
2018-01-07 19:54:53 -0800

    aa

--- Program/src/Program61.java
+++ Program/src/Program61.java
@@ -7,25 +7,20 @@ public class Program61 {
 		int initial;
 		int finalm;
 		int gallon;
-		int mpg;
-		int a;
-		int b;
+
 		System.out.print("Initial miles: ");
 		initial=scanner.nextInt();
-		if(initial<0) {
-			System.out.println("bye");
-			scanner.close();
-		}
-		else {
+		int mpg;
+		while(initial>0) {
+			System.out.print("Initial miles: ");
+			initial=scanner.nextInt();
 			System.out.print("Final miles: ");
 			finalm=scanner.nextInt();
 			System.out.print("Gallons: ");
 			gallon=scanner.nextInt();
-			a=finalm-initial;
-			mpg=a/gallon;
-			System.out.println("Miles per Gallon: " + mpg);
-			
-			}
+			mpg=(initial-finalm)/gallon;
+			System.out.println("Miles per gallon = " + mpg);
+		}
 			
 		}
 	}
