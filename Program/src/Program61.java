ed58ddc8ba43accfd7f2c05df8a1b39efc3d6c1f
Daniel Haneul Lim <Daniel Haneul Lim@DESKTOP-VRG1CC1>
2018-04-27 20:35:17 -0700

    hello

--- Program/src/Program61.java
+++ Program/src/Program61.java
@@ -4,26 +4,26 @@ public class Program61 {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Miles Per Gallon Program");
-		int initial;
+		int initial=1;
 		int finalm;
 		int gallon;
 
-		System.out.print("Initial miles: ");
-		initial=scanner.nextInt();
 		int mpg;
 		while(initial>0) {
 			System.out.print("Initial miles: ");
 			initial=scanner.nextInt();
+			if(initial<0) {
+				System.out.println("bye");
+				System.exit(0);
+			}
 			System.out.print("Final miles: ");
 			finalm=scanner.nextInt();
 			System.out.print("Gallons: ");
 			gallon=scanner.nextInt();
-			mpg=(initial-finalm)/gallon;
+			mpg=(finalm-initial)/gallon;
 			System.out.println("Miles per gallon = " + mpg);
 		}
-		if(initial<0) {
-			System.out.
-		}
+		
 			
 		}
 	}
